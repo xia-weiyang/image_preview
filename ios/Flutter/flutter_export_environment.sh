1fb64584e4215396b61a307b8152231e36ead6fc
xia-weiyang <gw@jiushig.com>
2021-03-13 16:23:34 +0800

    增加web目录

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/xia/develop/flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/xia/develop/work/image_preview"
+export "FLUTTER_ROOT=/home/xia/work/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/xia/work/project/image_preview"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/xia/develop/flutter/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
